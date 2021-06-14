75aacf0c35b99b7af6b7651e2d9a8bacedc5c94c
Rodrigo Zucchini <rodrigozucchini1997@gmail.com>
2021-06-14 13:24:20 -0300

    actualizacion de ejercicios de HTML

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -57,7 +57,7 @@ function filter(array) {
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
   return array.filter(function(string){
-    return string[0] === "a"
+    return string [0] === "a"
   })
 }
 
